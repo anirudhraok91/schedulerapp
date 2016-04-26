c9865e372034d7fdd94e08a9ed88312f48497dee
anirudhraok91 <rao.anirudhk@gmail.com>
2016-04-26 19:05:18 +0000

    Added lecture to navbar

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -4,5 +4,19 @@ class LecturesController < ApplicationController
   end
   
   def create
+      @lecture = Lecture.new(lecture_params)
+      
+      if @lecture.save
+          flash[:success] = 'Class added'
+          redirect_to new_lecture_path
+      else
+          flash[:danger] = 'Fail Da'
+          redirect_to new_lecture_path
+      end
   end
+  
+  private
+    def lecture_params
+        params.require(:lecture).permit(:subject, :day, :tutor)
+    end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,2 +1,4 @@
 class Lecture < ActiveRecord::Base
+    validates :subject, presence: true
+    validates :day, presence: true
 end
