14d267b5069ab07505dd61283536b30ce50ad587
anirudhraok91 <rao.anirudhk@gmail.com>
2016-04-27 23:43:40 +0000

    Plans added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427203823) do
+ActiveRecord::Schema.define(version: 20160427233529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20160427203823) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "day"
+  end
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
