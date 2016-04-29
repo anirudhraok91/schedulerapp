383366b9c504ab53fafbe0b096e0ccec4182a7b1
anirudhraok91 <rao.anirudhk@gmail.com>
2016-04-29 23:33:13 +0000

    Progress on profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,5 @@
+class ProfileController < ApplicationController
+    def new
+        @user = User.find(params[:user_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   get '/about' => 'pages#about'
   
   root 'pages#home'
