cbd5d096a41f5b10c93aa62f947f0cf5133be7da
anirudhraok91 <rao.anirudhk@gmail.com>
2016-05-02 20:21:44 +0000

    basic profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
-class ProfileController < ApplicationController
+class ProfilesController < ApplicationController
     def new
         @user = User.find(params[:user_id])
+        @profile = @user.build_profile
     end
 end
